9b1d3d30ec39b77d14c5276c23e3c513a3223d00
NBN7 <nqchossj@gmail.com>
2025-02-07 10:37:20 -0300

    update bg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Navbar, Footer } from "@/components";
-// import background from "@/assets/background.svg";
+import background from "@/assets/background.svg";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -45,11 +45,11 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body className="w-full flex flex-col min-h-dvh relative">
-        {/* <img
+        <img
           src={background.src}
           alt=""
           className="absolute top-0 left-0 w-full h-full -z-10 blur-[100px]"
-        /> */}
+        />
         <Navbar />
         <main className="flex-1 flex justify-center w-full">
           <div className="max-w-7xl w-full px-6">{children}</div>
