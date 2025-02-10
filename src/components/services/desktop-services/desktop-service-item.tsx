520b6db6b9ce296ae120c8b303a6764d01afc3cf
NBN7 <nqchossj@gmail.com>
2025-02-09 22:43:41 -0300

    update services

--- src/components/services/desktop-services/desktop-service-item.tsx
+++ src/components/services/desktop-services/desktop-service-item.tsx
@@ -5,15 +5,20 @@ interface DesktopServiceItemProps {
 }
 
 export const DesktopServiceItem = ({ item }: DesktopServiceItemProps) => {
+  const { Icon, TITLE, DESCRIPTION } = item;
+
   return (
     <div className="bg-foreground w-full h-96 p-4 border rounded-lg hover:border-blue-300 transition">
       <div className="w-full flex flex-col gap-4">
         <div className="flex items-center gap-2">
-          <item.Icon size={18} aria-label={`Icono de ${item.TITLE}`} />
-          <span className="font-semibold">{item.TITLE}</span>
+          <Icon size={18} aria-label={`Icono de ${TITLE}`} className="text-blue-400" />
+          <span className="font-semibold">{TITLE}</span>
         </div>
-        <p className="text-sm text-softgrey" aria-describedby={`service-desc-${item.TITLE}`}>
-          {item.DESCRIPTION}
+        <p
+          className="text-sm text-softgrey"
+          aria-describedby={`service-desc-${TITLE}`}
+        >
+          {DESCRIPTION}
         </p>
       </div>
     </div>
