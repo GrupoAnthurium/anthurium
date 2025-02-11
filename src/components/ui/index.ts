eefdddf262406a37269d6f173ec0689962fa27be
NBN7 <nqchossj@gmail.com>
2025-02-11 00:08:05 -0300

    add Input and Textarea components; update button variants and exports

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,3 +1,6 @@
 export * from "@/components/ui/button";
 export * from "@/components/ui/sheet";
 export * from "@/components/ui/carousel";
+export * from "@/components/ui/separator";
+export * from "@/components/ui/input";
+export * from "@/components/ui/textarea";
