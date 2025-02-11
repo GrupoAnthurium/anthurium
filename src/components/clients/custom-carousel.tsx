9e6ae70db9b659e0c19c2f395aed5d88817d1b17
NBN7 <nqchossj@gmail.com>
2025-02-10 21:43:59 -0300

    add custom carousel component and related UI elements

---
+++ src/components/clients/custom-carousel.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import Autoplay from "embla-carousel-autoplay";
+import { ClientItem } from "@/components";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components";
+import { TESTIMONIALS } from "@/constants";
+import type { TTestimonial } from "@/types";
+
+export const CustomCarousel = () => {
+  const renderClientItem = (testimonial: TTestimonial) => {
+    const { COMMENT, NAME, COMPANY } = testimonial;
+    return <ClientItem key={NAME} comment={COMMENT} name={NAME} company={COMPANY} />;
+  };
+
+  return (
+    <Carousel
+      className="mb-16"
+      plugins={[
+        Autoplay({
+          delay: 2500,
+        }),
+      ]}
+    >
+      <CarouselContent>{TESTIMONIALS.map(renderClientItem)}</CarouselContent>
+      <CarouselPrevious className="hidden 2xl:flex" />
+      <CarouselNext className="hidden 2xl:flex" />
+    </Carousel>
+  );
+};
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,2 +1,3 @@
 export * from "@/components/ui/button";
 export * from "@/components/ui/sheet";
+export * from "@/components/ui/carousel";
