41379a21a522e56cf416d3f9d72133aef52a7059
NBN7 <nqchossj@gmail.com>
2025-03-13 20:05:55 -0300

    refactor: update Anthurium component to include logo and adjust layout in Navbar

--- src/components/anthurium.tsx
+++ src/components/anthurium.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { twMerge } from "tailwind-merge";
+import Image from "next/image";
 
 interface AnthuriumProps {
   className?: string;
@@ -11,9 +12,17 @@ export const Anthurium = ({ className }: AnthuriumProps) => {
 
   return (
     <button
-      className={twMerge("font-semibold text-lg", className)}
+      className={twMerge("font-semibold text-lg flex items-center", className)}
       onClick={goUp}
     >
+      <Image
+        src="/white-logo-no-bg.png"
+        alt="Anthurium"
+        className="size-10 select-none"
+        draggable="false"
+        width={1200}
+        height={1200}
+      />
       Anthurium Group
     </button>
   );
--- src/components/layout/navbar/navbar.tsx
+++ src/components/layout/navbar/navbar.tsx
@@ -2,7 +2,6 @@
 
 import { useMediaQuery } from "@/hooks";
 import { MobileDrawer, MenuList, Anthurium } from "@/components";
-import Image from "next/image";
 
 export const Navbar = () => {
   const isMobile = useMediaQuery(640);
@@ -10,11 +9,7 @@ export const Navbar = () => {
   return (
     <nav className="w-full flex items-center justify-center h-16 border-b bg-softwhite sticky top-0 z-50">
       <div className="max-w-7xl w-full flex items-center justify-between px-6">
-        <div className="flex space-x-1">
-          <Image src="/white-logo-no-bg.png" alt="Anthurium" className="h-10 w-10" width={1200} height={1200} />
-          <Anthurium />
-        </div>
-
+        <Anthurium />
         {isMobile ? <MobileDrawer /> : <MenuList />}
       </div>
     </nav>
